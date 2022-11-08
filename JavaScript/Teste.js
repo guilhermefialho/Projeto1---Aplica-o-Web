bf1c07582513e441d8242fe3bc86a2c55632fb52
Gabriel Alves Developer <110769950+gabrielteg@users.noreply.github.com>
2022-11-08 09:14:48 -0300

    JS

---
+++ JavaScript/Teste.js
@@ -0,0 +1,11 @@
+const botaoAltura = document.getElementById ('botaoAltura');
+
+let alturaCliente = prompt ("Qual a Altura?");
+let LarguraCliente = prompt ("Qual a Largura?");
+let preçoCena1 = 129.99;
+
+function Altura () {
+alturaCliente
+}
+
+botaoAltura.addEventListener ( 'click', Altura )
