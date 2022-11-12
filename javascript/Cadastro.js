9cdaecc112bf84802d961d69196e281c525acfaa
guilhermefialho <92562186+guilhermefialho@users.noreply.github.com>
2022-11-12 12:27:37 -0300

    .

---
+++ javascript/Cadastro.js
@@ -0,0 +1,11 @@
+function onChangeName(){}     //criando funçoes para armazenagem de dados
+
+function onChangeEmail(){}
+
+function onPassword(){}
+
+function onChangeTelefone(){}
+
+function onChangeCEP(){}
+
+function onChangeEndereço(){}
