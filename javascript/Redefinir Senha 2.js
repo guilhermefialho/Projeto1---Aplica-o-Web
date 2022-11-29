b6ba6834baa21486edbbe72e6b7a755371cc629c
guilhermefialho <92562186+guilhermefialho@users.noreply.github.com>
2022-11-29 18:41:08 -0300

    .

---
+++ javascript/Redefinir Senha 2.js
@@ -0,0 +1,44 @@
+
+function redefinirSenha() {
+    email = document.getElementById('email2').value
+    firebase.auth().sendPasswordResetEmail(email).then(() =>
+
+
+        {
+            alert('Email de recuperação enviado com sucesso!');
+
+
+        }).catch(error => {
+
+        alert('Usuário ainda não cadastrado no sistema')
+    })
+
+
+}
+
+function ValidarRedefSenha() {
+    const emailValido = emailValido2();
+
+    document.getElementById('botao2').disabled = !emailValido;
+
+}
+
+function emailValido2() {
+
+    const email = document.getElementById("email2").value;
+
+    if (!email) {
+        return false;
+    }
+
+
+    return ValidarEmail(email);
+
+}
+
+
+function ValidarEmail(email) {
+
+    return /\S+@\S+\.\S+/.test(email);
+
+}
